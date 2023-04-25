2980e0bf79a7f892c842b8c4bf2593c7dea90e7e
gajevski <mikolajxgajewski@gmail.com>
2023-04-25 14:52:54 +0200

    Add DiscordConfiguration

--- Bot.cs
+++ Bot.cs
@@ -1,8 +1,10 @@
 ﻿using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.Interactivity;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +18,19 @@ namespace DiscordBot
         public CommandsNextExtension Commands { get; private set; }
         public async Task RunAsync()
         {
+            var json = string.Empty;
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
 
+            var configJson = JsonConvert.DeserializeObject<ConfigJSON>(json);
+
+            var config = new DiscordConfiguration()
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
         }
     }
 }
