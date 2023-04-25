7d0680b4309c00d0a741c38eb960a402242280a7
gajevski <mikolajxgajewski@gmail.com>
2023-04-25 14:44:47 +0200

    Dodaj pliki projektów.

---
+++ Bot.cs
@@ -0,0 +1,22 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public InteractivityExtension Interactivity { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+        public async Task RunAsync()
+        {
+
+        }
+    }
+}
